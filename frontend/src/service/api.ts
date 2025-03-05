dfa73ad9fa8d84dfc04e600610b55c9cc7e232ad
rubens-rodrigues <misterubens@gmail.com>
2025-03-05 11:45:47 -0300

    url

--- frontend/src/service/api.ts
+++ frontend/src/service/api.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api-bannersgc.onrender.com/api/banners"',
-    // baseURL: 'http://localhost:4000/api/banners',
+    baseURL: "https://api-bannersgc.onrender.com/api/banners", 
     headers: {
         'Content-Type': 'application/json',
     }
 });
 
 export default api;
+
+// baseURL: 'http://localhost:4000/api/banners',

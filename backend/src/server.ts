54d88102420a5d065d70aded27d61bca64a4ff9c
rubens-rodrigues <misterubens@gmail.com>
2025-03-05 11:43:38 -0300

    server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware para JSON
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
